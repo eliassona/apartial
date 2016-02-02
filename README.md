13ec7ea478a94de5f49cff152ea8b6044197bed8
Anders Eliasson <anders.eliasson@digitalroute.com>
2016-02-02 12:05:43 +0100

    working now

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
 # apartial
 
-A Clojure library designed to ... well, that part is up to you.
+A slightly more advanced partial
 
 ## Usage
+
+The regular clojure core partial  function will only let you partial initialize a function with the arguments from left to right. There is no
+way to skip a parameter in the middle. The apartial macro will let you do that by using _ (underscore) for the uninitialized arguments.
+An example
+
+```clojure
+=> (use 'apartial.core)
+=> (defn add-three-numbers [x0 x1 x2] (+ x0 x1 x2))
+;initialize x0 and x2 with apartial
+=> (def ap (apartial add-three-numbers 1 _ 3))
+=> (ap 2)
+6
+=> (ap 4)
+8
+
+```
 
 FIXME
 
--- src/apartial/core.clj
+++ src/apartial/core.clj
@@ -1,23 +1,12 @@
 (ns apartial.core)
 
-
 (defn _->name [l]
-  (map (fn [x1 x2] (if (= x1 '_) (with-meta (symbol (str "x" x2)) {:arg-type :formal}) x1)) l (-> l count range)))
+  (mapv (fn [x1 x2] (if (= x1 '_) (with-meta (symbol (str "x" x2)) {:arg-type :formal}) x1)) l (-> l count range)))
 
-(defn formal? [arg]
-  (= (-> arg meta :arg-type) :formal))
+(defn formal? [arg] (= (-> arg meta :arg-type) :formal))
 
 (defmacro apartial [f & args]
-  `(fn [~'& ~'as]
-     (println (str "args: " '~args))
-     ;(apply ~f (concat ~args ~'as))
-     )
-  )
-
-
-(comment 
-  (apartial + _ 2 _)
-  should be transformed to
-  (fn [x0 x1 & args]
-    (apply + (concat [x0 2 x1] args))) 
-  )
+  (let [actual (_->name args)
+        formal (filter formal? actual)]
+    `(fn [~@formal ~'& the-rest#]
+         (apply ~f (concat ~actual the-rest#)))))
