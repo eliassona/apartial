4e5ba34f34590f81d1331328a0bbf672cfaef90f
Anders Eliasson <anders.eliasson@digitalroute.com>
2016-02-26 11:21:07 +0100

    use 1.8

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject apartial "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An alternative to partial"
+  :url "https://github.com/eliassona/apartial"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
